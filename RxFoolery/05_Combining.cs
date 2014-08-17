c610f2b50610a7614dee6bc82e18daad8a3f9c8d
Sebastian Gebski <sebastian@gebski.net>
2014-08-17 09:55:39 +0200

    Very basic concat

--- RxFoolery/05_Combining.cs
+++ RxFoolery/05_Combining.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
 
 namespace RxFoolery
 {
     [TestClass]
     public class Combining
     {
+        private static IObservable<string> CreateSeq(IScheduler scheduler,
+                                                     double delay,
+                                                     double seconds,
+                                                     string prefix,
+                                                     int threshold)
+        {
+            return Observable.Interval(TimeSpan.FromSeconds(seconds),
+                                       scheduler)
+                             .Select(p => string.Format("{0}{1}",
+                                                        prefix,
+                                                        p))
+                             .Take(threshold);
+        }
+
         [TestMethod]
         public void Concat()
         {
-            Assert.Fail();
+            var scheduler = new TestScheduler();
+
+            var seq1 = CreateSeq(scheduler,
+                                 0,
+                                 1,
+                                 "A",
+                                 3);
+
+            var seq2 = CreateSeq(scheduler,
+                     0,
+                     0.7,
+                     "B",
+                     3);
+
+            var result = new List<string>();
+
+            seq1.Concat(seq2)
+                     .Subscribe(result.Add);
+
+            scheduler.Start();
+
+            Check.That(result)
+                 .ContainsExactly("A0","A1","A2","B0","B1","B2");
         }
 
         [TestMethod]
